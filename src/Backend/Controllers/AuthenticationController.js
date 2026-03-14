c09a9f5aeb2e0ce07718a54ef1209a58447594a2
Dhruv Shah <dhruvshah2717@gmail.com>
2026-03-14 08:07:33 +0530

    Update AuthenticationController.js

--- src/Backend/Controllers/AuthenticationController.js
+++ src/Backend/Controllers/AuthenticationController.js
@@ -86,7 +86,7 @@ exports.login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,              // Prevent JavaScript access (security)
             secure: true,               // Set true in production with HTTPS
-            sameSite: "None",             // Required for cross-origin
+            sameSite: "none",             // Required for cross-origin
             maxAge: 24 * 60 * 60 * 1000  // 24 hours
         });
 
